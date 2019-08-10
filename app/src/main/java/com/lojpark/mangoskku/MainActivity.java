52daeea6b8f49df5b9298f2c79a1bea07457a2e8
lojpark <lojpark@gmail.com>
2019-08-10 21:26:45 +0900

    Install jsoup library

--- app/src/main/java/com/lojpark/mangoskku/MainActivity.java
+++ app/src/main/java/com/lojpark/mangoskku/MainActivity.java
@@ -2,13 +2,65 @@ package com.lojpark.mangoskku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView1;
+    private String htmlPageUrl = "https://www.yna.co.kr/";
+    private String htmlContentInStringFormat="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setMovementMethod(new ScrollingMovementMethod());
+
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute();
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                Document doc = Jsoup.connect(htmlPageUrl).get();
+
+                Elements titles = doc.select("div.news-con h2.tit-news");
+
+                for(Element e: titles){
+                    System.out.println("title: " + e.text());
+                    htmlContentInStringFormat += e.text().trim() + "\n";
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textView1.setText(htmlContentInStringFormat);
+        }
     }
 }
